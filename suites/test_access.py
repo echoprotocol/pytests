f23401a40797c6d8c7a5a5f4b5ec23c60362a5a3
Kazak Aliaksei <a.kazak@pixelplex.io>
2018-12-12 10:45:15 +0300

    add simple test

---
+++ suites/test_access.py
@@ -0,0 +1,44 @@
+import json
+import os
+
+import lemoncheesecake.api as lcc
+from lemoncheesecake.matching import check_that_in, is_, is_integer
+
+RESOURCES_DIR = os.path.join(os.path.dirname(__file__), "..//resources")
+
+SUITE = {
+    "description": "Test 'ECHO'"
+}
+
+
+@lcc.suite("Simple test")
+class TestEcho:
+    def __init__(self):
+        self.database = json.load(open(os.path.join(RESOURCES_DIR, "echo_apis.json")))["DATABASE"]
+
+    @lcc.test("Get response from method")
+    def test_get_response(self, connection, login):
+        # Authorization status check and request data from the database
+        lcc.set_step("Requesting Access to an API")
+        if login:
+            connection.send(json.dumps(self.database))
+        else:
+            lcc.log_error("Login failed")
+
+        # Receive identifier
+        db = json.loads(connection.recv())
+        lcc.log_info("Received: \n{}".format(json.dumps(db, indent=4)))
+
+        # Check the validity of the response from the server
+        lcc.set_step("Check API response")
+        check_that_in(
+            db,
+            "id", is_integer(),
+            "jsonrpc", is_("2.0"),
+            "result", is_integer()
+        )
+
+        # Close connection to WebSocket
+        lcc.set_step("Close connection")
+        connection.close()
+        lcc.log_info("Connection closed ")
