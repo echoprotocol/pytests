914b1a108f1bc7daa9b52a45470a6769e1d7abf7
Kazak Aliaksei <a.kazak@pixelplex.io>
2018-12-17 10:07:20 +0300

    modified class BaseTest and project

--- project.py
+++ project.py
@@ -1,9 +1,12 @@
 import os.path
+import sys
 
 from lemoncheesecake.project import SimpleProjectConfiguration
 
 
 project_dir = os.path.dirname(__file__)
+sys.path.append(project_dir)
+
 project = SimpleProjectConfiguration(
     suites_dir=os.path.join(project_dir, "suites"),
     fixtures_dir=os.path.join(project_dir, "fixtures"),
